477792a04ae85ef7ef9e3268e05975650e0dcba8
Jacob Hardin-Bernhardt <jacob.hardin.bernhardt@nyu.edu>
2025-09-17 23:45:18 -0400

    counties data

--- homework1.R
+++ homework1.R
@@ -1,1 +1,10 @@
+#install.packages(descr)
+#library(descr)
+
 #R first hw
+counties<-read.csv("counties.csv")
+summary(counties)
+
+hist(counties$unemp_pct)
+#freq(counties$unemp_pct)
+#freq(counties$trump_pct)
